312ed9d8fad9c677b5328b067b5ff896a62e4108
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-13 08:05:05 -0300

    projeto base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+const App = () => {
+    return(
+        <Router>
+            <Routes>
+                <Route/>
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
